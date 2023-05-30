05aec6b08927fdead880705488ea1facdc05d228
Nadya Falaleeva <hopes91@users.noreply.github.com>
2023-05-30 15:17:33 +0300

    open and close search info

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// hide the top yellow bar
 const productButton = document.querySelector('.relevant-product_top button');
 
 const closeRelevantProduct = () => {
@@ -6,4 +7,23 @@ const closeRelevantProduct = () => {
     relevantProduct.classList.add('height-zero');
 };
 
+// toggle search bar's hidden content
+const searchBar = document.getElementById('search-bar');
+const hiddenButtons = document.querySelector('.hidden-buttons');
+const searchInfo = document.querySelector('.search-info-wrapper');
+
+const showSearchInfo = () => {
+    hiddenButtons.style.display = 'flex';
+    searchInfo.style.display = 'block';
+};
+
+const closeButton = document.getElementById('close-search');
+
+const closeSearchInfo = () => {
+    hiddenButtons.style.display = 'none';
+    searchInfo.style.display = 'none';
+};
+
+closeButton.addEventListener('click', closeSearchInfo);
+searchBar.addEventListener('focus', showSearchInfo);
 productButton.addEventListener('click', closeRelevantProduct);
